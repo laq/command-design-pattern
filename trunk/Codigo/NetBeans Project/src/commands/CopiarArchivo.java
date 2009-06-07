281496df8ea2ef1d75bdfe6702eef43c190b2abd
damontic <damontic@f9c53702-52fc-11de-9eea-55519c9c5eb7>
2009-06-07 03:50:36 +0000

    CopiarArchivo execute() acabado

--- trunk/Codigo/NetBeans Project/src/commands/CopiarArchivo.java
+++ trunk/Codigo/NetBeans Project/src/commands/CopiarArchivo.java
@@ -6,6 +6,14 @@
 package commands;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,8 +21,8 @@ import java.io.File;
  */
 public class CopiarArchivo implements Command {
 
-    private String nombre_archivo_origen;
-    private String nombre_archivo_destino;
+    private File nombre_archivo_origen;
+    private File nombre_archivo_destino;
 
     /**
      *
@@ -23,7 +31,7 @@ public class CopiarArchivo implements Command {
      * @param destino
      * @param destino_dir
      */
-    public CopiarArchivo(String origen, String destino) {
+    public CopiarArchivo(File origen, File destino) {
         nombre_archivo_origen = origen;
         nombre_archivo_destino = destino;
     }
@@ -31,16 +39,33 @@ public class CopiarArchivo implements Command {
     /**
      *
      */
-    public void execute() {
-
+    public boolean execute() {
         // crea los archivos
-        File srcDir = new File(nombre_archivo_origen);
-        File dstDir = new File(nombre_archivo_destino);
-
-        
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = new FileInputStream(nombre_archivo_origen);
+            out = new FileOutputStream(nombre_archivo_destino);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+        } catch (FileNotFoundException ex) {
+            return false;
+        } catch (IOException ioex) {
+            return false;
+        }
+        return true;
     }
 
-    public void undo() {
+    public boolean undo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Exception getLastException() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
