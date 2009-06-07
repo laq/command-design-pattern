7665a43b14c362f4f0a0c5ade86600bab305a891
damontic <damontic@f9c53702-52fc-11de-9eea-55519c9c5eb7>
2009-06-07 03:10:24 +0000

    CopiarCarpeta Creados los atributos de la clase los métodos getters y setters el constructor de la clase q inicializa los atributos

--- trunk/Codigo/NetBeans Project/src/commands/CopiarCarpeta.java
+++ trunk/Codigo/NetBeans Project/src/commands/CopiarCarpeta.java
@@ -9,7 +9,19 @@ package commands;
  *
  * @author david
  */
-public class CopiarCarpeta implements Command{
+public class CopiarCarpeta implements Command {
+
+    private String nombre_carpeta_origen;
+    private String nombre_carpeta_destino;
+    private String direccion_origen;
+    private String direccion_destino;
+
+    public CopiarCarpeta(String origen, String origen_dir, String destino, String destino_dir) {
+        nombre_carpeta_origen = origen;
+        nombre_carpeta_destino = origen_dir;
+        direccion_origen = destino;
+        direccion_destino = destino_dir;
+    }
 
     public void execute() {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -17,6 +29,62 @@ public class CopiarCarpeta implements Command{
 
     public void undo() {
         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    /**
+     * @return the nombre_carpeta_origen
+     */
+    public String getNombre_carpeta_origen() {
+        return nombre_carpeta_origen;
+    }
+
+    /**
+     * @param nombre_carpeta_origen the nombre_carpeta_origen to set
+     */
+    public void setNombre_carpeta_origen(String nombre_carpeta_origen) {
+        this.nombre_carpeta_origen = nombre_carpeta_origen;
+    }
+
+    /**
+     * @return the nombre_carpeta_destino
+     */
+    public String getNombre_carpeta_destino() {
+        return nombre_carpeta_destino;
+    }
+
+    /**
+     * @param nombre_carpeta_destino the nombre_carpeta_destino to set
+     */
+    public void setNombre_carpeta_destino(String nombre_carpeta_destino) {
+        this.nombre_carpeta_destino = nombre_carpeta_destino;
+    }
+
+    /**
+     * @return the direccion_origen
+     */
+    public String getDireccion_origen() {
+        return direccion_origen;
+    }
+
+    /**
+     * @param direccion_origen the direccion_origen to set
+     */
+    public void setDireccion_origen(String direccion_origen) {
+        this.direccion_origen = direccion_origen;
+    }
+
+    /**
+     * @return the direccion_destino
+     */
+    public String getDireccion_destino() {
+        return direccion_destino;
+    }
+
+    /**
+     * @param direccion_destino the direccion_destino to set
+     */
+    public void setDireccion_destino(String direccion_destino) {
+        this.direccion_destino = direccion_destino;
     }
 
 }
