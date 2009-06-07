0bc8fdda97ae4445469a066a6390fd9fd360aaa2
sergio.bobillier <sergio.bobillier@f9c53702-52fc-11de-9eea-55519c9c5eb7>
2009-06-07 02:38:59 +0000

    Primer import de proyecto de NetBeans

---
+++ Codigo/NetBeans Project/src/commands/Command.java
@@ -0,0 +1,17 @@
+package commands;
+
+/** La interfaz que deben implementar todos los comandos del proyecto.
+ *
+ *  @author Sergio
+ */
+
+public interface Command
+{
+    /** El método que se llama para que el comando sea ejecutado */
+
+    public void execute();
+
+    /** El método que se llama para que el comando sea deshecho */
+    
+    public void undo();
+}
