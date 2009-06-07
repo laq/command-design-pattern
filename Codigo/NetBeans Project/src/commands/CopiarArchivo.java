f7f95e58d99a3c429bafe309327686f57cbddcf8
damontic <damontic@f9c53702-52fc-11de-9eea-55519c9c5eb7>
2009-06-07 04:32:10 +0000

    CopiarArchivo todo cambiado por el cambio de las exceptions

--- Codigo/NetBeans Project/src/commands/CopiarArchivo.java
+++ Codigo/NetBeans Project/src/commands/CopiarArchivo.java
@@ -20,7 +20,6 @@ public class CopiarArchivo implements Command {
 
     private File nombre_archivo_origen;
     private File nombre_archivo_destino;
-    private Exception ex;
 
     /**
      *
@@ -30,55 +29,27 @@ public class CopiarArchivo implements Command {
     public CopiarArchivo(File origen, File destino) {
         nombre_archivo_origen = origen;
         nombre_archivo_destino = destino;
-        ex = null;
     }
 
     /**
      *  Ejecuta la copia de un archivo a otro
-     *  @return true si se realizó con éxito<br />
-     *          false si no se realizó con éxito
      */
-    public boolean execute() {
-        try{
-            FileInputStream fis = new FileInputStream(nombre_archivo_origen);
-            FileOutputStream fos = new FileOutputStream(nombre_archivo_destino);
-            FileChannel canalFuente = fis.getChannel();
-            FileChannel canalDestino = fos.getChannel();
-            canalFuente.transferTo(0, canalFuente.size(), canalDestino);
-            fis.close();
-            fos.close();
-        }catch (FileNotFoundException ex) {
-            this.ex = ex;
-            return false;
-        }catch (IOException ioex) {
-            this.ex = ioex;
-            return false;
-        }
-        return true;
+    public void execute() throws FileNotFoundException, IOException {
+        FileInputStream fis = new FileInputStream(nombre_archivo_origen);
+        FileOutputStream fos = new FileOutputStream(nombre_archivo_destino);
+        FileChannel canalFuente = fis.getChannel();
+        FileChannel canalDestino = fos.getChannel();
+        canalFuente.transferTo(0, canalFuente.size(), canalDestino);
+        fis.close();
+        fos.close();
     }
 
     /**
      * Borra el archivo creado si lo encuentra.
-     * @return true si se realizó con éxito<br />
-     *         false si no se realizó con éxito
      */
-    public boolean undo() {
-        try {
-            // borra archivo destino
-            Eliminadora.deleteFile(nombre_archivo_destino);
-        } catch (FileNotFoundException ex) {
-            this.ex = ex;
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Retirna la excepción encontrada
-     * @return Reported Exception
-     */
-    public Exception getLastException() {
-        return ex;
+    public void undo() throws FileNotFoundException {
+        // borra archivo destino
+        Eliminadora.deleteFile(nombre_archivo_destino);
     }
 
 }
