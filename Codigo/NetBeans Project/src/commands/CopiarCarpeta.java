be8c5fec6744a9c7902872c25f058f4c9cf84df1
damontic <damontic@f9c53702-52fc-11de-9eea-55519c9c5eb7>
2009-06-07 03:00:12 +0000

    CopiarCarpeta Se creó la clase CopiarCarpeta q implementa Command

---
+++ Codigo/NetBeans Project/src/commands/CopiarCarpeta.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package commands;
+
+/**
+ *
+ * @author david
+ */
+public class CopiarCarpeta implements Command{
+
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void undo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
