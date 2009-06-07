1a26629407fa399507aec96e8a23b80e8ad38caa
laqp14 <laqp14@f9c53702-52fc-11de-9eea-55519c9c5eb7>
2009-06-07 05:05:00 +0000

    geters y seters de la clase

--- Codigo/NetBeans Project/src/commands/CrearAccesoDirecto.java
+++ Codigo/NetBeans Project/src/commands/CrearAccesoDirecto.java
@@ -13,4 +13,32 @@ import java.io.File;
 public abstract class CrearAccesoDirecto implements Command {
     private File executable;
     private File lugarDelAcceso;
+
+    /**
+     * @return the executable
+     */
+    public File getExecutable() {
+        return executable;
+    }
+
+    /**
+     * @param executable the executable to set
+     */
+    public void setExecutable(File executable) {
+        this.executable = executable;
+    }
+
+    /**
+     * @return the lugarDelAcceso
+     */
+    public File getLugarDelAcceso() {
+        return lugarDelAcceso;
+    }
+
+    /**
+     * @param lugarDelAcceso the lugarDelAcceso to set
+     */
+    public void setLugarDelAcceso(File lugarDelAcceso) {
+        this.lugarDelAcceso = lugarDelAcceso;
+    }
 }
