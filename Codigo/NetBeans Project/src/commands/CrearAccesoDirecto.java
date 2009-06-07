bc988bc0ffd346e27587143a196404b7d9472a41
laqp14 <laqp14@f9c53702-52fc-11de-9eea-55519c9c5eb7>
2009-06-07 03:09:44 +0000

    Clase abstracta crear acceso directo, se hizo abstracta para q los dos valores en comun esten, no se si este bien o sea mejor una interfaz

---
+++ Codigo/NetBeans Project/src/commands/CrearAccesoDirecto.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commands;
+
+import java.io.File;
+
+/**
+ *
+ * @author LAQ
+ */
+public abstract class CrearAccesoDirecto implements Command {
+    private File executable;
+    private File lugarDelAcceso;
+}
