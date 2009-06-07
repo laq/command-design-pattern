b69822a1700099c091c26ff976f28a0a7ef6dff5
damontic <damontic@f9c53702-52fc-11de-9eea-55519c9c5eb7>
2009-06-07 04:42:15 +0000

    CopiarArchivoTest Pasa las preubas

---
+++ Codigo/NetBeans Project/test/commands/CopiarArchivoTest.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package commands;
+
+import java.io.File;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author david
+ */
+public class CopiarArchivoTest {
+
+    public CopiarArchivoTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of execute method, of class CopiarArchivo.
+     */
+    @Test
+    public void testExecute() throws Exception {
+        System.out.println("execute");
+        File src = new File("/home/david/Escritorio/Tareas Pendientes");
+        File dest = new File("/home/david/Escritorio/copia_TP");
+        CopiarArchivo instance = new CopiarArchivo(src, dest);
+        instance.execute();
+        // TODO review the generated test code and remove the default call to fail.
+        if(!dest.exists()){
+            System.out.println("Error al copiar");
+        }
+    }
+
+    /**
+     * Test of undo method, of class CopiarArchivo.
+     */
+    @Test
+    public void testUndo() throws Exception {
+        System.out.println("undo");
+        File src = new File("/home/david/Escritorio/Tareas Pendientes");
+        File dest = new File("/home/david/Escritorio/copia_TP");
+        CopiarArchivo instance = new CopiarArchivo(src, dest);
+        instance.undo();
+        // TODO review the generated test code and remove the default call to fail.
+        if(!dest.exists()){
+            System.out.println("Error al borrar");
+        }
+    }
+
+}
