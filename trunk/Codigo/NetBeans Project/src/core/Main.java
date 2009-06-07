137218b9bbf5104f3a7aa705795e9cfb43c5fad3
sergio.bobillier <sergio.bobillier@f9c53702-52fc-11de-9eea-55519c9c5eb7>
2009-06-07 06:09:04 +0000

    Commit del main para resolver el conflicto

--- trunk/Codigo/NetBeans Project/src/core/Main.java
+++ trunk/Codigo/NetBeans Project/src/core/Main.java
@@ -5,17 +5,22 @@
 
 package core;
 
-/**
+import gui.Wizard;
+
+/** Clase principal del programa. Punto de entrada a la apliación.
  *
- * @author Sergio
+ *  @author Sergio
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
+    /** Método de entrada.
+     *
+     *  @param args Argumentos de la línea de comandos
      */
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+        Wizard wiz;
+        wiz = new Wizard();
+        wiz.setVisible(true);        
     }
-
 }
