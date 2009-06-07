671be56f4420fabefad1c755400faa7973f44f61
laqp14 <laqp14@f9c53702-52fc-11de-9eea-55519c9c5eb7>
2009-06-07 04:53:54 +0000

    clase crear carpeta y su test estan practicamente afuera 90% confiabilidad :P

---
+++ trunk/Codigo/NetBeans Project/test/commands/CrearCarpetaTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package commands;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Administrador
+ */
+public class CrearCarpetaTest {
+
+    public CrearCarpetaTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of execute method, of class CrearCarpeta.
+     */
+    @Test
+    public void testExecute() {
+        try {
+            System.out.println("execute");
+            //prueba valida
+            CrearCarpeta instance = new CrearCarpeta(new File("/XXX"));
+            instance.execute();
+            //prueba  invalida
+            instance = new CrearCarpeta(new File("XXX/"));
+            instance.execute();
+            // TODO review the generated test code and remove the default call to fail.
+            //fail("The test case is a prototype.");
+        } catch (FileNotFoundException ex) {
+            System.out.println("Exc"+ex);
+            Logger.getLogger(CrearCarpetaTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of undo method, of class CrearCarpeta.
+     */
+    @Test
+    public void testUndo() throws Exception {
+        System.out.println("undo");
+        CrearCarpeta instance = new CrearCarpeta(new File("XXX/"));;
+        instance.undo();
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+}
